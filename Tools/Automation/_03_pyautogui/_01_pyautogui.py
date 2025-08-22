8ceb7a4f60c3fede3ab905d32ff5e7f4f58cd73c
srogsrogi <han970112@gmail.com>
2025-08-22 17:51:03 +0900

    pyautogui 내용추가

--- Tools/Automation/_03_pyautogui/_01_pyautogui.py
+++ Tools/Automation/_03_pyautogui/_01_pyautogui.py
@@ -1,24 +1,26 @@
 # pyautogui : 키보드, 마우스 제어
 import pyautogui
 
-print(pyautogui.size()) # 모니터사이즈
-print(pyautogui.position()) # 커서 위치
+# print(pyautogui.size()) # 모니터사이즈
+# print(pyautogui.position()) # 커서 위치
 
-# pyautogui.mouseInfo() # 실시간 커서위치, 컬러값 등 제공
+pyautogui.mouseInfo() # 실시간 커서위치, 컬러값 등 제공
 
 
 # 커서 절대좌표로 이동
-# pyautogui.moveTo(1700,500, duration = 0.3) # duration에 난수를 넣는다면?
+pyautogui.moveTo(1700,500, duration = 0.3) # duration에 난수를 넣는다면?
+pyautogui.moveTo(130,500, duration = 0.3)
+pyautogui.moveTo(0,0, duration = 0.3)
 
 # 현재 커서 위치 기준 상대적으로 이동
-# pyautogui.move(-300,-300, duration = 2)
+pyautogui.move(-300,-300, duration = 2)
 
 # 클릭
-# pyautogui.click(button = 'right') # default = 'left'
+pyautogui.click(button = 'right') # default = 'left'
 
 # 잠시 대기
 import time
-# time.sleep(1)
+time.sleep(1)
 # pyautogui.move(-30,200,duration=1)
 # pyautogui.click()
 
@@ -32,12 +34,14 @@ import time
 # pyautogui.click(*notepad_coord_2)
 
 # 텍스트 작성하기. 한글은 안 됨
-# pyautogui.write('hi~', interval = 0.1)
+pyautogui.write('hi~', interval = 0.1)
 
 # 한글입력은 이렇게 클립보드에 복사한 다음 가져오는 형식으로 해야 함
 import pyperclip
-# pyperclip.copy('안녕')
-# pyautogui.hotkey('ctrl','v')
+pyperclip.copy('안녕')
+pyautogui.hotkey('ctrl','v')
+
+
 
 # 커서 대기하다가 알림창 뜨자마자 확인 누르기
 x, y = 102, 785   # 확인 버튼 좌표
